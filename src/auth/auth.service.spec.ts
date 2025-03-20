f4e6636080a4f7320839f2c16be14787be767478
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-20 17:41:22 +0530

    trying auth service test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,40 @@
+import { mock } from 'node:test';
+import { beforeEach, describe, expect, it } from '@jest/globals';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from '../auth/schemas/user.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { LoginDTO } from './dto/login.dto';
+import { signupDTO } from './dto/signup.dto';
+import { create } from 'domain';
+import { find } from 'rxjs';
+
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let model: Model<User>;
+
+  const mockUser = {
+    _id: '61c0ccf11d7bf83d153d7c06',
+    name: 'Aryan',
+    email: 'aryan1@gmail.com',
+  };
+
+  const mockAuthService = {
+    create: jest.fn(),
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
