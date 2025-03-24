9632ff8cc112d1bb3e68aeb7eae58e7f19902407
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-24 19:34:37 +0530

    added test cases for auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,96 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { AuthController } from './auth.controller';
+// import { AuthService } from './auth.service';
+// import { User } from './schemas/user.schema';
+// import { getModelToken } from '@nestjs/mongoose';
+
+// describe('AuthController', () => {
+//   let controller: AuthController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [AuthController],
+//     }).compile();
+
+//     controller = module.get<AuthController>(AuthController);
+//   });
+
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { User } from 'src/auth/schemas/user.schema';
+import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+
 describe('AuthController', () => {
-  let controller: AuthController;
+
+  //mocking the services/providers
+  let authservice: AuthService;
+  let authcontroller: AuthController;
+
+  const mockUser = {
+    _id: '61c0ccf11d7bf83d153d7c06',
+    name: 'Ghulam',
+    email: 'ghulam1@gmail.com',
+  };
+
+  let jwtToken='jwtToken';
+
+  const mockAuthService = {
+    signUp: jest.fn().mockResolvedValueOnce(jwtToken),
+    login: jest.fn().mockResolvedValueOnce(jwtToken)
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authservice = module.get<AuthService>(AuthService);
+    authcontroller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authcontroller).toBeDefined();
   });
+
+  describe('signUp', () => {
+    it('should register a new user', async () => {
+      const signUpDto = {
+        username: 'Ghulam',
+        email: 'ghulam1@gmail.com',
+        password: '12345678',
+      };
+
+      const result = await authcontroller.signUp(signUpDto);
+      expect(authservice.signUp).toHaveBeenCalled();
+      expect(result).toEqual(jwtToken);
+    });
+  });
+
+  describe('login',()=>{
+    it('should login a user',async ()=>{
+      const loginDTO={
+        email:"test@gmail.com",
+        password:"12345678"
+      }
+
+      const result=await authcontroller.login(loginDTO);
+      expect(authservice.login).toHaveBeenCalled();
+      expect(result).toEqual(jwtToken);
+    })
+  })
 });
