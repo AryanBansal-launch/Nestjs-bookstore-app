00e82a6cc09fabc34ec9c6e90c65eb2b95df04cc
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-28 17:56:39 +0530

    added cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors(
+    {
+      origin: "*"
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
